f39d7a31f6786849d41b7e53ce7e5e3523647053
YUsandesu <miaomiaoyum@outlook.com>
2025-03-16 03:46:50 +0800

    Update tilling.py

--- new_tiling/tilling.py
+++ new_tiling/tilling.py
@@ -1,37 +1,32 @@
-from new_tiling.BruijnsSystem import BruijnsSystem
+import BruijnsSystem
 from the_control import *
 from PY5_2DToolkit import *
-from BruijnsSystem import BruijnsSystem
 import pandas as pd
 
 def setup():
     py5.size(500, 500)
-    s_v= sd.screen_axis(0,0)
-    for seg_line in tilling_data:
-        A_P,B_P=tool.point_shift(seg_line,s_v)
-        tool.Segmentline_drop(A_P, B_P)
+    start_location= sd.screen_axis(0,0)
     load()
     slider('num1', [50, py5.height - 120], value=0, range_val=[0, 10],size=[400,30])
-    slider('num2', [50, py5.height - 80], value=0, range_val=[0, 1000], size=[400, 30])
+    for k,v in tool.point_dic.items():
+        tool.point_dic[k]=tool.point_shift(v,start_location)
+
 def draw():
     global  tilling_data
-    s_v = sd.screen_axis(0, 0)
     py5.background(155)
     back = slider_value()
     if back is not None:
-        tool.reset()
-        tilling_data = till.create_tilling(the_p, num=back['num1']+back['num2'])
-        for seg_line in tilling_data:
-            A_P, B_P = tool.point_shift(seg_line, s_v)
-            tool.Segmentline_drop(A_P, B_P,color=py5.color(0,0,0,90))
-
+        print('changed')
     sd.screen_draw_SegmentLine(tool.get_Segmentline_dic(),0)
 
 if __name__ == "__main__":
-    sd = Screen_draw(py5)
     tool = Tools2D()
-    till = BruijnsSystem(sides=5, max_num_of_line=30)
-    the_p = till.interaction_data_line_id[(0, 0)][60]
-    print(f'选取交点:{the_p}')
-    tilling_data = till.create_tilling(the_p, num=0)
+    a = BruijnsSystem.BruijnsSystem(shifted_distance=5)
+    till = a.tilling
+    seg_group = till.unique_tilling(till._center_point)
+
+    for i in seg_group:
+        tool.Segmentline_drop(i[0],i[1],color=py5.color(0,0,0,100))
+
+    sd = Screen_draw(py5)
     py5.run_sketch()
