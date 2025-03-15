4ed51d28165818269239f7ef30f5039d86c8e42e
YUsandesu <miaomiaoyum@outlook.com>
2025-03-16 04:50:32 +0800

    Update tilling.py

--- new_tiling/tilling.py
+++ new_tiling/tilling.py
@@ -2,12 +2,13 @@ import BruijnsSystem
 from the_control import *
 from PY5_2DToolkit import *
 import pandas as pd
-
+start_location:list
 def setup():
+    global start_location
     py5.size(500, 500)
     start_location= sd.screen_axis(0,0)
     load()
-    slider('num1', [50, py5.height - 120], value=0, range_val=[0, 10],size=[400,30])
+    slider('num1', [50, py5.height - 120], value=0, range_val=[0, 1000],size=[400,30])
     for k,v in tool.point_dic.items():
         tool.point_dic[k]=tool.point_shift(v,start_location)
 
@@ -16,15 +17,23 @@ def draw():
     py5.background(155)
     back = slider_value()
     if back is not None:
-        print('changed')
+        i_seg_group = till.WFS(back['num1'])
+        tool.reset()
+        for seg in i_seg_group:
+            tool.Segmentline_drop(seg[0], seg[1], color=py5.color(0, 0, 0, 100))
+        for k, v in tool.point_dic.items():
+            tool.point_dic[k] = tool.point_shift(v, start_location)
+
     sd.screen_draw_SegmentLine(tool.get_Segmentline_dic(),0)
 
 if __name__ == "__main__":
     tool = Tools2D()
-    a = BruijnsSystem.BruijnsSystem(shifted_distance=5)
+    a = BruijnsSystem.BruijnsSystem(shifted_distance=6,origin_norm=20,max_num_of_line=1000)
+    print('Gird-finish')
     till = a.tilling
-    seg_group = till.unique_tilling(till._center_point)
-
+    print('Tilling-finish')
+    seg_group = till.WFS(0)
+    print('WFS-finish')
     for i in seg_group:
         tool.Segmentline_drop(i[0],i[1],color=py5.color(0,0,0,100))
 
